0049a39a8967590b510b248283648fdf9685c3a6
Jamie Sharpe <github@jamiesharpe.co.uk>
2014-06-05 22:20:30 +0100

    docs

--- ColourDetector/ColourDetector/KeyValuePair.cs
+++ ColourDetector/ColourDetector/KeyValuePair.cs
@@ -2,9 +2,21 @@
 
 namespace ColourDetector
 {
+    /// <summary>
+    /// A key/value generic wrapper that can be serialised
+    /// for XML storage.
+    /// </summary>
+    /// <typeparam name="TK">Type of key</typeparam>
+    /// <typeparam name="TV">Type of Value</typeparam>
     [Serializable]
     public class KeyValuePair<TK, TV>
     {
+        #region Properties
+        public TK Key { get; set; }
+        public TV Value { get; set; }
+        #endregion Properties
+
+        #region Constructors
         public KeyValuePair()
         {
         }
@@ -14,8 +26,6 @@ namespace ColourDetector
             this.Key = k;
             this.Value = v;
         }
-
-        public TK Key { get; set; }
-        public TV Value { get; set; }
+        #endregion Constructors
     }
 }
